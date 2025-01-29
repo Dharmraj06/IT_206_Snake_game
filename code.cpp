5ac1741bcf626f87e88be5bdf1b526f63c5242b0
dhvanit1123 <dhvanit525@gmail.com>
2025-01-29 23:00:33 +0530

    Add files via upload

--- code.cpp
+++ code.cpp
@@ -3,4 +3,35 @@
 #include<conio.h>
 using namespace std;
 
-void setUp
+const int width = 20;
+const int hieght = 20;
+bool gameover;
+int x,y;
+int fx,fy;
+int score;
+int ntail;
+enum eDirection {LEFT,RIGHT,UP,DOWN};
+eDirection dir;
+
+
+class setUp{
+
+    gameover = false;
+    x = width/2;
+    y = hieght/2;
+    fx = rand() % width;
+    fy = rand() % hieght;
+    score = 0;
+
+};
+
+int main(){
+
+    /*
+    
+        HELLO WORLD!
+
+     */
+
+    return 0;
+}
