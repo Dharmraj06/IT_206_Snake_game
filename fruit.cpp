7890956143dbede4c4acce760833eeaa25a29d17
KNANY68 <9f52yuvansh.kesh@gmail.com>
2025-01-29 23:06:21 +0530

    fruit

---
+++ fruit.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+
+using namespace std;
+int score;
+class fruit()
+{
+    public:
+    int FruitX, FruitY, score=-10;
+
+    void getCoordinate()
+    {
+        do(
+
+            FruitX = rand() % width;
+            FruitY = rand() % height;
+            score+=10;
+        )
+        while (matrix[FruitX][FruitY] == 'O');
+    }
+
+    void spawn()
+    {
+        matrix[FruitX][FruitY] = 'F';
+    }
+
+
+
+}
