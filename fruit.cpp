a024c962b28d2f21dc18db598f352fe3599a2f7f
KNANY68 <9f52yuvansh.kesh@gmail.com>
2025-01-29 23:51:28 +0530

    comments

--- fruit.cpp
+++ fruit.cpp
@@ -1,49 +1,56 @@
 #include<iostream>
-#include <cstdlib>  // For rand() and srand()
+#include <cstdlib>  
 #include <ctime>
 
 using namespace std;
-int score;
-class fruit
+
+class fruit()
 {
     
     public:
+
+    //allocating coordinates for Fruit(F)//
     void getCoordinateF()
     {
         do(
-
             FruitX = rand() % width;
             FruitY = rand() % height;
             score+=10;
         )
         while (matrix[fruitX][fruitY] == 'O' || matrix[FruitX][FruitY] == 'O');
     }
+
+    //allocating coordinates for fruit(f)//
     void getCoordinatef()
     {
-        do(
-
+        do( 
             fruitX = rand() % width;
             fruitY = rand() % height;
             score+=10;
         )
         while (matrix[fruitX][fruitY] == 'O' || matrix[FruitX][FruitY] == 'O');
     }
+
+    //spawning Fruit(F)
     void spawnF()
-    {
+    {   
         matrix[FruitX][FruitY] = 'F';
     }
+
+    //spawning Fruit(f)
     void spawnf()
     {
         matrix[FruitX][FruitY] = 'f';
     }
 
+    //choosing fruit and spawning it
     void getFruit()
     {
-        int randNum = rand() % 3;
+        int randNum = rand() % 2;
         if(randNum=0)
         {
             getCoordinateF();
-            spawnF()
+            spawnF();
         }
         if(randNum=1)
         {
@@ -53,4 +60,4 @@ class fruit
 
     }
 
-};
+}
